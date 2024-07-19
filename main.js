7172b905b72e6dfe5d4fd4dab14c380ec875e3d5
Nastya-20 <62868214+Nastya-20@users.noreply.github.com>
2024-07-19 14:39:29 +0300

    add

---
+++ main.js
@@ -0,0 +1,40 @@
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+// Функция отображения кнопки при прокрутке
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) { // Если прокрутка больше 300 пикселей
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+// Функция прокрутки вверх при нажатии на кнопку
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Плавная прокрутка
+    });
+});
+
+
+// Закриття та відкриття мобильного меню
+const mobMenu = document.querySelector('.mob-menu');
+const openModalBtn = document.querySelector('.open-menu-btn');
+const closeModalBtn = document.querySelector('.button-modal-close');
+
+function openMenu(menu) {
+    console.log('Opening menu:', menu);
+    menu.classList.add('is-open');
+}
+
+function closeMenu(menu) {
+    console.log('Closing menu:', menu);
+    menu.classList.remove('is-open');
+}
+
+// Обработчики событий для открытия и закрытия мобильного меню
+openModalBtn.addEventListener('click', () => openMenu(mobMenu));
+closeModalBtn.addEventListener('click', () => closeMenu(mobMenu));
+
+

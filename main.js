a619e796e64a76bb6180eebc9bd28b62ea236312
Nastya-20 <62868214+Nastya-20@users.noreply.github.com>
2024-07-21 15:27:38 +0300

    add

--- main.js
+++ main.js
@@ -26,15 +26,50 @@ const closeModalBtn = document.querySelector('.close-menu-btn ');
 function openMenu(menu) {
     console.log('Opening menu:', menu);
     menu.classList.add('is-open');
+    body.classList.add('no-scroll');
 }
 
 function closeMenu(menu) {
     console.log('Closing menu:', menu);
     menu.classList.remove('is-open');
+    body.classList.remove('no-scroll');
 }
 
 // Обработчики событий для открытия и закрытия мобильного меню
 openModalBtn.addEventListener('click', () => openMenu(mobMenu));
 closeModalBtn.addEventListener('click', () => closeMenu(mobMenu));
+
+// Відкриття форми на комп'ютері
+const modalForm = document.querySelector('.modal');
+const openOrderServiceButton = document.querySelector('.button');
+const closeOrderServiceButton = document.querySelector('.button-modal-close');
+const modalOverlay = document.querySelector('.modal-overlay');
+const body = document.querySelector('body');
+
+function openForm() {
+  console.log('Opening form');
+  modalForm.classList.add('is-open');
+  modalOverlay.classList.add('is-open');
+  body.classList.add('no-scroll');
+}
+
+function closeForm() {
+  console.log('Closing form');
+  modalForm.classList.remove('is-open');
+  modalOverlay.classList.remove('is-open');
+  body.classList.remove('no-scroll');
+}
+
+openOrderServiceButton.addEventListener('click', openForm);
+closeOrderServiceButton.addEventListener('click', closeForm);
+
+// Close the modal when clicking on the overlay
+modalOverlay.addEventListener('click', (event) => {
+  if (event.target === modalOverlay) {
+    closeForm();
+  }
+});
+
+
 
 
